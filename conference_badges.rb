435ede8e8df26c8af946a34c3e912b6be6e83f46
christian iverson <chris.w.iverson@gmail.com>
2020-12-08 15:14:31 -0800

    commit tree a04a73920a518225c5914ba9f68b48cbce5fa29d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker (name)
+    return "Hello, my name is #{name}."
+end
+def batch_badge_creator (array)
+    new_array=[]
+    array.each do|names|
+       new_array.push"Hello, my name is #{names}."
+    end
+    new_array
+end
+def assign_rooms (speaker)
+    room_array=[]
+    counter=1
+    speaker.each do |speakers|
+        room_array.push("Hello, #{speakers}! You'll be assigned to room #{counter}!")
+        counter += 1
+    end
+    room_array
+end
+def printer (people)
+    people.each do |name|
+        puts "Hello, my name is #{name}."
+    end
+    counter=1
+    people.each do |name|
+        puts "Hello, #{name}! You'll be assigned to room #{counter}!"
+        counter+=1
+    end
+end
